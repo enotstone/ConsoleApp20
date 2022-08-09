0ada5eaa6435ee0977099c487eaafdc844dc47a7
enot demiurge <enotstone@ya.ru>
2022-08-09 16:51:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        delegate double MyDelegate(double R);
+        static void Main()
+        {
+            Console.WriteLine("Input Radius");
+            double R = Convert.ToDouble(Console.ReadLine());
+
+            MyDelegate myDel = VBall;
+            myDel += LRound;
+            myDel += SRound;
+            
+            myDel(R);
+            
+            Console.ReadKey();
+        }
+        static double LRound(double r)
+        {
+            double resAc = 2 * Math.PI * r;
+            Console.WriteLine("Long round = {0}",resAc);
+            return resAc;
+        }
+        static double SRound(double r)
+        {
+            double resAc = Math.PI * r * r;
+            Console.WriteLine("Area of ​​a circle = {0}", resAc);
+            return resAc;
+        }
+        static double VBall(double r)
+        {
+            double resAc = 4 * Math.PI * (r * r * r) / 3;
+            Console.WriteLine("Ball volume = {0}", resAc);
+            return resAc; 
+        }
+    }
+}
